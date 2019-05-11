6590e2de5273cfd71286dd8bd6cf0b6d131b30e1
sharonmaswai <chepmaswai@gmail.com>
2019-05-11 22:55:30 +0300

    create class user, user details and instaces for the calss.

---
+++ user.py
@@ -0,0 +1,17 @@
+import pyperclip
+class User:
+
+    '''
+    A class in which instances can be addedself.
+
+    '''
+
+    user_info =[]
+    def __init__(self,first_name,surname,username,email_address,password):
+        self.first_name=first_name
+        sel.surname=surname
+        self.username=username
+        self.email_address=email
+        self.password=password
+
+        
