3e09a178c98354a5c42fb8c9c2294587dca1c518
sharonmaswai <chepmaswai@gmail.com>
2019-05-13 12:04:21 +0300

    edit run.py code

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from user import User
-from user import credentials
-import sys
+from user import Credentials
+
 
 def create_account(username,password):
-    new_user_account=User(username,password)
-    return new_user_account
+
+  new_user_account=User(username,password)
+  return new_user_account
 
 
 def save_users_credentials(user):
@@ -32,7 +33,7 @@ def create_new_credentials(platform,username,password):
     '''
     This function creates a new locked accounts
     '''
-    new_user_credentials=Credentials(platform,username,password)
+    new_user_credentials=Credentials(platforrm,username,password)
 
 def save_credentials():
     '''
@@ -42,46 +43,53 @@ def save_credentials():
     Credentials.save_credentials()
 
 def main():
-    print("Hello!!\n Welcome.")
-    print("\n")
-    name=input("What is your name?")
-    username=input("Create your username:")
-    password=input("Now create your unique password:")
-    print(f " Welcome {username! Please select the next step.")
+    user_name=input("Hello! Welcome to Password locker.\n Tell us your name. ")
+    print("Hello {}, what do you want to do?".format(user_name))
+
+
+
+
+
 
     while True:
-        save_credentials(create_new_credentials(platform,username,password))
 
-        print("Select any of the following short codes:1- Create a new account, 2-Find a user, 3- Exit" )
-        if short_code == '1':
+
+        short_code=input("Select any of the following short codes: \n ca- Create a new account \n fu-Find a user \n x- Exit:" )
+
+
+
+        if short_code == "ca":
+
+
            print("New account")
            platform = input("Enter platform i.e facebook, instagram, twitter etc:")
 
-           print("Usename:")
+           print("Username:")
            username = input()
 
-           print("Password:")
-           password = input()
+           print("Enter your password:")
+           password= input()
 
-           save_credentials(create_account(username,password))
-           print('\n')
-           print(f "Your {platform} credential have been received and saved.")
+           save_response=input("Would you like to save your account credentials?: y/n \n")
+           if save_response=="y":
+               
+               print ("Welcome {}, you have successfully created a {} account: \n Here are your credentials: {} \n {}".format(username,platform, username,password))
 
-        elif short_code =='2':
+        elif short_code =='fu':
            print("Enter the username of the account you wish to find:")
 
            search_username = input()
            if find_user_by_name(search_user):
               search_user = find_username(search_username)
-              print(f "{search_user.platform} {search_user.username")
+              print("{} {}".format(search_user.platform,search_user.username))
 
-              print(f "Password...{search_user.password}")
+              print( "Password:{}".format(search_user))
 
            else:
             print("The user does not exist.")
 
-        elif short_code=='3':
-            print(f "Goodbye, {username}")
+        elif short_code=='x':
+            print("Goodbye, {}".format(user_name))
 
 
 if __name__ == '__main__':
